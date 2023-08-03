2340c22462da479121be3ef5022d2b43e6c74a17
Igor Streshnev <igor.streshnev2019@gmail.com>
2023-08-03 23:30:03 +0300

    first 3 fragments

---
+++ app/src/main/java/com/monke/triviamasters/ui/modesFeature/ModesFragment.kt
@@ -0,0 +1,21 @@
+package com.monke.triviamasters.ui.modesFeature
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.monke.triviamasters.R
+
+class ModesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_modes, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/monke/triviamasters/ui/questionFeature/DescriptionFragment.kt
@@ -0,0 +1,24 @@
+package com.monke.triviamasters.ui.questionFeature
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.monke.triviamasters.R
+
+
+class DescriptionFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_description, container, false)
+    }
+
+
+}
