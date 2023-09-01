4161bfba84c3e5481a1fae05dccabbb7a2d783c9
Igor Streshnev <igor.streshnev2019@gmail.com>
2023-09-01 20:26:43 +0300

    disabled button

--- app/src/main/java/com/monke/triviamasters/ui/loginFeature/LoginFragment.kt
+++ app/src/main/java/com/monke/triviamasters/ui/loginFeature/LoginFragment.kt
@@ -1,6 +1,8 @@
 package com.monke.triviamasters.ui.loginFeature
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -26,12 +28,13 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        loginComponent = (activity?.application as App).appComponent.loginComponent().create()
+
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loginComponent = (activity?.application as App).appComponent.loginComponent().create()
         navController = view.findNavController()
     }
 
--- app/src/main/java/com/monke/triviamasters/ui/loginFeature/createPlayerFeature/CreatePlayerFragment.kt
+++ app/src/main/java/com/monke/triviamasters/ui/loginFeature/createPlayerFeature/CreatePlayerFragment.kt
@@ -1,14 +1,19 @@
 package com.monke.triviamasters.ui.createPlayerFeature
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnFocusChangeListener
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
@@ -17,6 +22,7 @@ import com.monke.triviamasters.R
 import com.monke.triviamasters.databinding.FragmentCreatePlayerBinding
 import com.monke.triviamasters.ui.loginFeature.LoginFragment
 import com.monke.triviamasters.ui.loginFeature.createPlayerFeature.CreatePlayerViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -27,6 +33,7 @@ class CreatePlayerFragment : Fragment() {
     lateinit var factory: CreatePlayerViewModel.Factory
     private val viewModel: CreatePlayerViewModel by viewModels { factory }
     private var binding: FragmentCreatePlayerBinding? = null
+    private lateinit var navController: NavController
 
 
     override fun onCreateView(
@@ -39,23 +46,47 @@ class CreatePlayerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val navController = (activity as MainActivity).mainNavController
-
+        navController = (activity as MainActivity).mainNavController
         (parentFragment?.parentFragment as LoginFragment).loginComponent.inject(this)
 
+        setupSignInButton()
+        setupUsernameEditText()
+    }
+
+    private fun setupUsernameEditText() {
+        binding?.editTextUsername?.setText( viewModel.playerUsername.value)
+        binding?.editTextUsername?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(text: Editable?) {
+
+                viewModel.setUsername(text?.toString() ?: "")
+
+                binding?.btnSignIn?.isEnabled = text?.toString()?.isNotEmpty() ?: false
+            }
+
+        })
         binding?.editTextUsername?.setOnFocusChangeListener { _, hasFocus ->
             if (!hasFocus) {
                 binding?.editTextUsername?.let {
-                    viewModel.setUsername(it.text.toString())
-                }
 
+                }
             }
         }
+    }
 
+    private fun setupSignInButton() {
         binding?.btnSignIn?.setOnClickListener {
             navController.navigate(R.id.action_loginFragment_to_mainFragment)
             viewModel.savePlayer()
         }
+        binding?.btnSignIn?.isEnabled = viewModel.playerUsername.value.isNotEmpty()
     }
 
     override fun onDestroy() {
